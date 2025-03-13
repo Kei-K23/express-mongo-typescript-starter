1cbf52abf0ebad76b1cc7f84c60210702bad46f9
Kei-K23 <keiksl2301@gmail.com>
2025-03-13 22:57:36 +0630

    feat: setup logger config and logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import morgan from 'morgan';
+import logger from './config/logger.config';
 
 const app = express();
 
+// Logger middleware for HTTP request
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }),
+);
+
 app.get('/', (req, res) => {
-  res.json({ message: 'HELLO' });
+  res.json({ message: 'ERROR' });
 });
 
 export { app };
